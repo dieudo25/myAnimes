13f18b0942b5825e0c993b549ffecab15e3460fd
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2022-01-18 14:49:55 +0100

    add animeReducer for list and detail

---
+++ app/src/state/reducers/animeReducer.js
@@ -0,0 +1,47 @@
+import { AnimeActionTypes } from "../constants/action-types";
+
+const initialState = {
+    ainmes: [],
+    loading: true
+}
+
+
+export const ainmeReducer = (state=initialState, { type, payload }) => {
+    // For animes list
+    switch(type) {
+        case AnimeActionTypes.SET_ANIMES:
+            return {
+                ...state,
+                ainmes: payload,
+                loading: false
+            };
+        case AnimeActionTypes.AINME_ERRORS:
+            return {
+                error: payload,
+                loading: false
+            };
+        default:
+            return state;
+    }
+}
+
+export const selectedAnimeReducer = (state={}, {type, payload}) => {
+    // For anime detail
+    switch (type) {
+        case AnimeActionTypes.SELECTED_ANIME:
+            return {
+                ...state,
+                ...payload,
+                loading: false
+            };
+        case AnimeActionTypes.AINME_ERRORS:
+            return {
+                error: payload,
+                loading: false,
+            };
+        case AnimeActionTypes.REMOVE_SELECTED_ANIME:
+            return {};
+        default:
+            return state;
+    }
+}
