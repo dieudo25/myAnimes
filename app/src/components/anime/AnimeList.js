47723b0df4e0fb875d1f5d599d31055b74132cdb
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2022-01-20 00:30:21 +0100

    update animeList component

--- app/src/components/anime/AnimeList.js
+++ app/src/components/anime/AnimeList.js
@@ -1,37 +1,34 @@
 import React, { useEffect } from "react";
 import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { setAnimes } from '../../state/actions/animeActions';
 import { AnimeActionTypes } from "../../state/constants/action-types";
-import AnimeContainer from "./AnimeListContainer";
+import AnimeListContainer from "./AnimeListContainer";
 
 const AnimeList = () => {
-    const animesState = useSelector(state => state.allAnimes);
     const dispatch = useDispatch();
-    const headers = {
-        "Content-Type": "application/json",
-    }
-    const ANIME_LIST_URL = "https://api.mangadex.org/manga"
-
-    const fetchAnimesList = async () => {
-        const response = await axios
-            .get(ANIME_LIST_URL, { headers: headers })
-            .catch((err) => {
-                dispatch({
-                    type: AnimeActionTypes.ANIME_ERRORS,
-                    payload: err,
-                })
-                console.log("Error", err);
-            });
-            dispatch(setAnimes(response.data));
-    };
 
     useEffect(() => {
+        const ANIME_LIST_URL = "https://api.mangadex.org/manga";
+
+        const fetchAnimesList = async () => {
+            const response = await axios
+                .get(ANIME_LIST_URL)
+                .catch((err) => {
+                    dispatch({
+                        type: AnimeActionTypes.ANIME_ERRORS,
+                        payload: err,
+                    })
+                }
+            );
+            dispatch(setAnimes(response.data));
+        };
+
         fetchAnimesList();
     }, []);
 
-   return <AnimeContainer />;
+   return <AnimeListContainer />;
 }
 
 export default AnimeList;
