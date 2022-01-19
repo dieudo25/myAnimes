8a24e3156cfc51a23320f9bf726b5889ffcac574
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2022-01-20 00:29:07 +0100

    update Nav style

--- app/src/components/layout/Header.js
+++ app/src/components/layout/Header.js
@@ -4,7 +4,7 @@ const Header = () => (
     <section className="nav-section py-5 bg-main-500 text-white text-center">
         <div className="container max-w-[960px] m-auto">
             <div className="nav-logo">
-                <h2>MyAnimes</h2>
+                <h2 className="text-[40px]">MyAnimes</h2>
             </div>
         </div>
     </section>
