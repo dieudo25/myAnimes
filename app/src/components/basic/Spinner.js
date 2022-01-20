fd25dd3bfe61a0306e81b08444f4fcbcb76c322e
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2022-01-20 17:48:22 +0100

    Add react-thunk and rect-spinner

--- app/src/components/basic/Spinner.js
+++ app/src/components/basic/Spinner.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { BallTriangle } from "react-loader-spinner"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const Spinner = () => (
-    <>
-        <span className="visually-hidden">Loading...</span>
-    </>  
+    <section className="section-spinner grid items-center m-auto w-[80px] h-[400px]">
+        <div className="h-[80px]">
+            <BallTriangle color="#9F3636" height={80} width={80} />
+        </div>
+    </section> 
 );
 
 export default Spinner;
--- app/src/state/store.js
+++ app/src/state/store.js
@@ -1,15 +1,19 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import rootReducers from "./reducers/index";
 
 const initialSate = {};
 
+// For monitoring the store from the browser
+// Do not forget to install Redux Dev Tools extension on the browser
+const reduxDevToolsExtention = window.__REDUX_DEVTOOLS_EXTENSION__;
+const reduxDevToolsExtentionFunciton= window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancer = compose(reduxDevToolsExtention, reduxDevToolsExtentionFunciton)
 const store = createStore(
     rootReducers,
     initialSate,
-
-    // For monitoring the store from the browser
-    // Do not forget to install Redux Dev Tools extension on the browser
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+    applyMiddleware(thunk),
+    //composeEnhancer    
 );
 
 export default store;
